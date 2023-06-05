e0a9e2b0906d30ee5d15520424932cf1eae79ed0
unknown <iyanvermilian@gmail.com>
2023-06-05 20:05:50 +0700

    add file

---
+++ 2 Variable & Data Type/0-index.js
@@ -0,0 +1,9 @@
+var nama = "Mark"
+var harga = 16000
+var apakahSudahMenikah = false
+var apakahSudahPunyaAnak
+
+console.log(nama)
+console.log(harga)
+console.log(apakahSudahMenikah)
+console.log(apakahSudahPunyaAnak)
---
+++ 2 Variable & Data Type/1-1-variable.js
@@ -0,0 +1,21 @@
+let namaVariabel1 = "hello";
+const namaVariabel2 = "world";
+
+// Contoh penulisan nama variabel yang benar
+
+let nama;
+let tempatTinggal;
+let _nomorHandphone;
+let $nomorAkunBank;
+// Contoh penulisan nama variabel yang salah
+
+// let makanan pembuka; //tidak boleh ada spasi
+// let @username; //tidak boleh diawali dengan @
+// let delete; //delete itu merupakan kata kunci di JavaScript
+
+
+// TASK 
+// Deklarasikan variabel animal dengan keyword let dan lalu akhiri dengan titik koma;
+
+// Tulis kode kalian di bawah ini
+let animal;
